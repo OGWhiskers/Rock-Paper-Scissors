64c0a72795355c518b9727e48e93b854f6384ae3
OGWhiskers <bayandadlamini02@gmail.com>
2022-11-22 21:53:41 +0200

    Add draft of function that plays single round
    Receives input from user choice of rock, paper or scissor then shows
    output of who won between user and computer. However it is case
    sensitive. Will later be used in loop of five rounds.

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 function getComputerChoice() {
 
-    let pcChoice = ['Rock', 'Paper', 'Scissor'];
+    let pcChoice = ['Rock', 'Paper', 'Scissors'];
 
     let randomPcChoice = pcChoice[Math.floor(Math.random()*pcChoice.length)];
 
-    console.log(randomPcChoice);
-    return getComputerChoice;
+    // console.log(randomPcChoice);
+    return randomPcChoice;
 }
 
 // getComputerChoice();
 
+function rpsRound(playerSelection, computerSelection) {
+
+    playerSelection = prompt('Rock, Paper or Scissors?');
+    console.log(playerSelection);
+
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
+    if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        console.log('You win! Rock beats Scissors');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        console.log('You lose! Paper beats Rock');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Rock') {
+        console.log('Its a draw!');
+    }
+    
+
+    else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        console.log('You win! Scissors beats Paper');
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        console.log('You lose! Rock beats Scissor');
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
+        console.log('Its a draw');
+    }  
+    
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log('You win! Paper Beats Rock');
+    }  else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        console.log('You lose! Scissors beats Paper ');
+    }  else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
+        console.log('Its a draw!');
+    }  
+    else {
+        console.log('Something is wrong');
+    }
+
+}
+
+rpsRound();
 
