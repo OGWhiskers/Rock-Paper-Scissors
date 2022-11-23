a9d49755bb8643c70edcecd76ace7b6293e2dab2
OGWhiskers <bayandadlamini02@gmail.com>
2022-11-23 20:41:41 +0200

    Add draft game function of whole game

--- script.js
+++ script.js
@@ -13,52 +13,59 @@ function getComputerChoice() {
 function rpsRound(playerSelection, computerSelection) {
 
     playerSelection = prompt('Rock, Paper or Scissors?');
-    console.log(playerSelection);
+   
 
     let playerSelectionTwo = playerSelection.toLowerCase();
 
-    console.log(playerSelectionTwo);
+   
 
     computerSelection = getComputerChoice();
-    console.log(computerSelection);
 
     let computerSelectionTwo = computerSelection.toLowerCase();
-    console.log(computerSelectionTwo);
+   
 
     if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'scissors') {
-        console.log('You win! Rock beats Scissors');
+      return('You win! Rock beats Scissors');
     } else if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'paper') {
-        console.log('You lose! Paper beats Rock');
+        return('You lose! Paper beats Rock');
     } else if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'rock') {
-        console.log('Its a draw!');
+        return('Its a draw!');
     }
     
 
     else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'paper') {
-        console.log('You win! scissors beats paper');
+        return('You win! Scissors beats Paper');
     } else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'rock') {
-        console.log('You lose! Rock beats Scissor');
+        return('You lose! Rock beats Scissor');
     } else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'scissors') {
-        console.log('Its a draw');
+        return('Its a draw');
     }  
     
     else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'rock') {
-        console.log('You win! paper Beats Rock');
+        return('You win! Paper Beats Rock');
     }  else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'scissors') {
-        console.log('You lose! Scissors beats paper ');
+        return('You lose! Scissors beats Paper ');
     }  else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'paper') {
-        console.log('Its a draw!');
+        return('Its a draw!');
     }  
-    else {
-        console.log('Something is wrong');
+     else {
+        return('Unrecognized Choice');
     }
 
 }
 
-rpsRound(); 
+// rpsRound(); 
+console.log(rpsRound());
+
+
 // Try make above function more efficient with switch statements.
-// Need to make it case insensitive
 
-
+function game() {
+    
+    for(let i = 0; i < 5; i++) {
+       console.log(rpsRound());
+    }
+}
+game();
 
 
