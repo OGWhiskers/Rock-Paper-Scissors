62fce5c68a1f5761db98de0d4543bbd9ddb13832
OGWhiskers <bayandadlamini02@gmail.com>
2022-11-24 16:41:43 +0200

    Add variables that store user and computer choice
    Intend to use these variables to create function or variable that keeps
    score of both computer and user.

--- script.js
+++ script.js
@@ -6,62 +6,75 @@ function getComputerChoice() {
 
     // console.log(randomPcChoice);
     return randomPcChoice;
+    // console.log(randomPcChoice);
 }
 
-// getComputerChoice();
+
 
 function rpsRound(playerSelection, computerSelection) {
 
     playerSelection = prompt('Rock, Paper or Scissors?');
    
-
     let playerSelectionTwo = playerSelection.toLowerCase();
 
-   
+    let rock = playerSelectionTwo === 'rock';
 
+    let scissor = playerSelectionTwo === 'scissors';
+
+    let paper = playerSelectionTwo === 'paper'
+
+    
     computerSelection = getComputerChoice();
 
     let computerSelectionTwo = computerSelection.toLowerCase();
-   
 
-    if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'scissors') {
+    let rockCom =  computerSelectionTwo === 'rock';
+    let scissorCom = computerSelectionTwo === 'scissors';
+
+    let paperCom = computerSelectionTwo === 'paper';
+
+
+    if ( rock && scissorCom) {
       return('You win! Rock beats Scissors');
-    } else if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'paper') {
+    } else if (rock && paperCom) {
         return('You lose! Paper beats Rock');
-    } else if (playerSelectionTwo == 'rock' && computerSelectionTwo == 'rock') {
+    } else if (rock && rockCom) {
         return('Its a draw!');
     }
     
 
-    else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'paper') {
+    else if (scissor && paperCom) {
         return('You win! Scissors beats Paper');
-    } else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'rock') {
+    } else if (scissor && rockCom) {
         return('You lose! Rock beats Scissor');
-    } else if (playerSelectionTwo == 'scissors' && computerSelectionTwo == 'scissors') {
+    } else if (scissor && scissorCom) {
         return('Its a draw');
     }  
     
-    else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'rock') {
+    else if (paper && rockCom) {
         return('You win! Paper Beats Rock');
-    }  else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'scissors') {
+    }  else if (paper && scissorCom) {
         return('You lose! Scissors beats Paper ');
-    }  else if (playerSelectionTwo == 'paper' && computerSelectionTwo == 'paper') {
+    }  else if (paper && paperCom) {
         return('Its a draw!');
     }  
      else {
         return('Unrecognized Choice');
     }
 
+
 }
 
 // rpsRound(); 
-console.log(rpsRound());
+// console.log(rpsRound());
 
 
 // Try make above function more efficient with switch statements.
+// Maybe create separate function to keep score:
+
 
 function game() {
-    
+
     for(let i = 0; i < 5; i++) {
        console.log(rpsRound());
     }
