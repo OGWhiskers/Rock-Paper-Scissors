91e912a7e0e96fd2c5124f68787e9fa88b75e4a9
OGWhiskers <bayandadlamini02@gmail.com>
2022-11-26 09:40:07 +0200

    Add draft single round function
    Function completes one round of rock, paper, scissors.

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
+// Computer Choice: ###########################################
+
+
 function getComputerChoice() {
     const com = ['Rock', 'Paper', 'Scissors']
 
     let comChoice = com[Math.floor(Math.random()* com.length)]
 
     let comChoiceAction = comChoice.toLowerCase();
-    console.log(comChoiceAction);
+    return(comChoiceAction);
 }
-getComputerChoice();
+console.log(getComputerChoice());
+
+// Computer Choice End ###########################################
+
+// Single Round ###########################################
+
+function singleRound(playerSelection,computerSelection) {
+
+    playerSelection = prompt('Rock, Paper or Scissors?');
+
+    let playerSelectionTwo = playerSelection.toLowerCase();
+ 
+    // New Variables to work with
+    let Rock = playerSelectionTwo === 'rock';
+    let Paper = playerSelectionTwo === 'paper';
+    let Scissors = playerSelectionTwo === 'scissors';
+
+    let ComRock = getComputerChoice() === 'rock';
+    let ComScissor = getComputerChoice ()=== 'scissors';
+    let ComPaper = getComputerChoice() === 'paper';
+
+    if (getComputerChoice() === 'rock') {
+        // Bookmark
+    }
+
+    // Dashboard Function
+    function dashboard(){
+        let UserScore = 0;
+        let ComScore = 0;
+
+        if (Rock && ComScissor || Paper && ComRock || Scissors && ComPaper ){
+            let UserScoreAdd = `Your current score is: ${UserScore + 1}`;
+            console.log(UserScoreAdd);
+        } else if (ComRock && Scissors || ComPaper && Rock || ComScissor && Paper) {
+            let ComScoreAdd = `Computer has a score of ${ComScore + 1} points`;
+            console.log(ComScoreAdd);
+        } else {
+            // console.log(UserScore+ ' ' + ComScore);
+        }   
+    }
+    dashboard();
+
+
+
+
+
+
+}
+singleRound();
